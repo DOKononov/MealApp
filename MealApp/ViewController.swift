b613275cd87d4e8c37bfa654258e506c3361e556
d.o.kononov <d.o.kononov@gmail.com>
2025-02-04 10:44:08 +0300

    Initial Commit

---
+++ MealApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MealApp
+//
+//  Created by Dmitry Kononov on 4.02.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
